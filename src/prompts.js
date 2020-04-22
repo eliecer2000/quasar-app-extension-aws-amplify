326b51aece989d3b6ad0e5057f4b8e3502941705
unknown <eliecer2000@gmail.com>
2020-04-22 15:00:10 -0500

    Maquina - master - 22042020_1500

--- src/prompts.js
+++ src/prompts.js
@@ -8,37 +8,64 @@
  * https://www.npmjs.com/package/inquirer#question
  *
  * Example:
+ *
+ * 
 
-  return [
-    {
-      name: 'name',
-      type: 'string',
-      required: true,
-      message: 'Quasar CLI Extension name (without prefix)',
-    },
-    {
-      name: 'preset',
-      type: 'checkbox',
-      message: 'Check the features needed for your project:',
-      choices: [
-        {
-          name: 'Install script',
-          value: 'install'
-        },
-        {
-          name: 'Prompts script',
-          value: 'prompts'
-        },
-        {
-          name: 'Uninstall script',
-          value: 'uninstall'
-        }
-      ]
-    }
-  ]
-
- */
+return [
+  {
+    name: 'name',
+    type: 'string',
+    required: true,
+    message: 'Quasar CLI Extension name (without prefix)',
+  },
+  {
+    name: 'preset',
+    type: 'checkbox',
+    message: 'Check the features needed for your project:',
+    choices: [
+      {
+        name: 'Install script',
+        value: 'install'
+      },
+      {
+        name: 'Prompts script',
+        value: 'prompts'
+      },
+      {
+        name: 'Uninstall script',
+        value: 'uninstall'
+      }
+    ]
+  }
+]
+*/
 
 module.exports = function () {
-  return []
-}
+  return [
+    {
+      name: "name",
+      type: "string",
+      required: true,
+      message: "Quasar CLI Extension name (without prefix)",
+    },
+    {
+      name: "preset",
+      type: "checkbox",
+      message: "Check the features needed for your project:",
+      choices: [
+        {
+          name: "Install script",
+          value: "install",
+        },
+        {
+          name: "Prompts script",
+          value: "prompts",
+        },
+        {
+          name: "Uninstall script",
+          value: "uninstall",
+        },
+      ],
+    },
+  ];
+};
